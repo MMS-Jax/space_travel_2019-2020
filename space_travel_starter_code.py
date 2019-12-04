500df0bcf298efcb6ff37136bef4d5d572cc4d4a
mr-kelley-mms <kelleyr@duvalschools.org>
2019-12-04 09:28:51 -0500

    Initial Commit for Starter Code

---
+++ space_travel_starter_code.py
@@ -0,0 +1,19 @@
+# Space Travel Calculator by <YOUR FIRST NAME and LAST INITIAL> on <DATE YOU WRITE THE CODE>. 
+
+# First declare variables for MILLION, BILLION, and TRILLION.  These will be used to convert distances later in the program. 
+
+million = 1000000
+billion = ? # Replace the ? with the correct value for billion. 
+trillion = ? # Replace the ? with the correct value for trillion. 
+
+# Next we need to declare the variables that will store the AVERAGE distances to each object in the Solar system.  Use Kilometers (Km) for your unit of measure.
+# Mars is 225 million Km from Earth on average.  So we create a variable dist_to_mars and set it equal to 225 multiplied by the million variable. 
+dist_to_mars = 255 * million 
+print("The distance to Mars is", dist_to_mars, " ilometers.") # Use this to check the math is correct. 
+
+# Create two more variables for objects INSIDE OUR SOLAR SYSTEM.  Make sure to convert the distances using million, billion, or trillion.  print() the variables 
+# check that they are correct. 
+
+dist_to_? =
+dist_to_? =
+
