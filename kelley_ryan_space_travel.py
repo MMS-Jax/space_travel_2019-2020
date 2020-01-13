954ca83f1a361fa0490ff39c094cfaa5b991bdc8
Ryan Kelley <kelleyr@duvalschools.org>
2020-01-13 12:44:36 -0500

    Created Version 0.0
    Added comment with author info, explained what program's purpose with print() commands.

---
+++ kelley_ryan_space_travel.py
@@ -0,0 +1,3 @@
+# Space Travel Calculator, Ryan Kelley, 01/13/20, Version 0.0 
+print("Greetings, I am the NASA Space Travel Calculator.")
+print("I will calculate whether a mission to outerspace will take more or less than three years.")
---
+++ kelley_ryan_space_travel_IDLE.py
@@ -0,0 +1,2 @@
+print("Greetings, I am the NASA Space Travel Calculator.")
+print("I will calculate whether a mission to outerspace will take more or less than three years.")
