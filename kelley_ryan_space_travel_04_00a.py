90be8cba88f42dbc035a9c0c003370619b833cfc
Ryan Kelley <kelleyr@duvalschools.org>
2020-01-14 13:26:18 -0500

    Version 0.1
    Added print() with instructions.

--- kelley_ryan_space_travel_04_00a.py
+++ kelley_ryan_space_travel_04_00a.py
@@ -1,1 +1,2 @@
-# Space Travel Calculator, Ryan Kelley, 01/14/20 1:08PM, Version 0.0 
+# Space Travel Calculator, Ryan Kelley, 01/14/20 1:08PM, Version 0.1 
+print("Greetings, I am the NASA Space Travel Calculator. I will help you calculate travel times to different places in space.") 
--- kelley_ryan_space_travel_IDLE.py
+++ kelley_ryan_space_travel_IDLE.py
@@ -1,4 +1,2 @@
-# Space Travel Calculator, Ryan Kelley, Code Written 01/14/20 12:16PM, Version 0.1
-print("Hello, I am the Space-Travel Calcubot.  I will determine whether or not you might die during outerspace travel.")  
-name = input("By what name do you want to be called? [Type your name and press ENTER.]")
-print (name)
+# Space Travel Calculator, Ryan Kelley, 01/14/20 1:08PM, Version 0.0 
+print"   p'jiosndfasbdfbniDFIUHI23URHIRH89P23UY498AFSDF FASDFASF[UJO;FNOSDFIUHSDAFAF F89PF89SFUAF89AYSHF9   ")
