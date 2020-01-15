0263a85785c2e284560af661578a33695ebad9ab
Ryan Kelley <kelleyr@duvalschools.org>
2020-01-15 15:18:41 -0500

    Version 0.1
    Added instructions with print()

--- kelley_ryan_space_travel_IDLE.py
+++ kelley_ryan_space_travel_IDLE.py
@@ -1,4 +1,2 @@
-# Space Travel Calculator, Ryan Kelley, 01/15/20 1:09PM, Version 0.1
-print("Greetings!  I am the Space Travel Calculator Bot 5000.  I will help determine whether you will die in outerspace or make it successfully to your destination.")
-name = input("What's your name? [Type your name and press ENTER.] ")
-print(name)
+# Space Travel Calculator, Ryan Kelley, 01/15/20 14:54PM, Version 0.0 
+pint("  ]opkgd0fjgdfg98djg9daj   ")
