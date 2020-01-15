70860ed067b86c4f7530d879a1aed42638c21ce1
Ryan Kelley <kelleyr@duvalschools.org>
2020-01-15 13:30:53 -0500

    Version 0.1
    Added print() for instructions.

--- kelley_ryan_space_travel_IDLE.py
+++ kelley_ryan_space_travel_IDLE.py
@@ -1,4 +1,2 @@
-# Space Travel Calculator, Ryan Kelley, 01/15/20 12:11PM, Version 0.1 
-print("This is the NASA Space Travel Calculator.  I will assist you in calculating travel times to outerspace locations.")
-my_name = input("What is your name? [Type your name and press ENTER key.]")
-print(my_name)
+# Space Travel Calculator, Ryan Kelley, 01/15/20 1:09PM, Version 0.0
+print("kl;jsdfio;jasdofjasdoi;fjfo;ja;  ")
