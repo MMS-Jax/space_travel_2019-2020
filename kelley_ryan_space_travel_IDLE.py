44bd5510166906b9b4a6c87d1e9348dff54f2bdf
Ryan Kelley <kelleyr@duvalschools.org>
2020-01-15 12:44:20 -0500

    Version 0.2
    Added input() to store username data.

--- kelley_ryan_space_travel_IDLE.py
+++ kelley_ryan_space_travel_IDLE.py
@@ -1,2 +1,4 @@
-# Space Travel Calculator, Ryan Kelley, 01/15/20 12:11PM, Version 0.0 
-prit("opiadfgj89035u890uguiohnui9p3h598uegiudfnguiph8935ukanr89ph8934u")
+# Space Travel Calculator, Ryan Kelley, 01/15/20 12:11PM, Version 0.1 
+print("This is the NASA Space Travel Calculator.  I will assist you in calculating travel times to outerspace locations.")
+my_name = input("What is your name? [Type your name and press ENTER key.]")
+print(my_name)
