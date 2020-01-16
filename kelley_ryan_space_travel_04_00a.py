79dfb633944589bc0bcb1d7fc1f1a073bfccf5a5
Ryan Kelley <kelleyr@duvalschools.org>
2020-01-16 13:35:02 -0500

    Version 0.3
    Added variables to store distances and millions/billions value.

--- kelley_ryan_space_travel_04_00a.py
+++ kelley_ryan_space_travel_04_00a.py
@@ -1,4 +1,18 @@
-# Space Travel Calculator, Ryan Kelley, 01/14/20 1:08PM, Version 0.25
+# Space Travel Calculator, Ryan Kelley, 01/14/20 1:08PM, Version 0.3
 print("Greetings, I am the NASA Space Travel Calculator. I will help you calculate travel times to different places in space.") 
 name = input("How are you!  What is your name? [Type your name and press ENTER.]")
 print("Good afternoon," + name)
+
+# Millions and Billions 
+million = 1000000
+billion = 1000000000
+
+
+# Distances for Objects in our Solar System [Measured in kilometers.] 
+distance_mars = 225 * million  
+distance_pluto = 5.9 * billion 
+distance_saturn = 1.4 * billion 
+
+print("The distance to Mars is" + distance_mars)
+print("The distance to Pluto is" + distance_pluto)
+print("The distance to Saturn is" + distance_saturn)
