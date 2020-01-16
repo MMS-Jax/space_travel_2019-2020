57c912ea049ce7bb21581694250f320b03cdfe13
Ryan Kelley <kelleyr@duvalschools.org>
2020-01-16 15:08:44 -0500

    Version 0.1
    Used print() to display instructions.

--- kelley_ryan_space_travel_06_03.py
+++ kelley_ryan_space_travel_06_03.py
@@ -1,1 +1,2 @@
-# Space Travel Calculator, Ryan Kelley, 01/16/20 2:51PM, Version 0.0
+# Space Travel Calculator, Ryan Kelley, 01/16/20 2:51PM, Version 0.1
+print("This is the Space Travel Calculator. I help determine if you can travel to a certain location in outerspace safely.")  
