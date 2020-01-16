39930da8b03e00ec257b79d8044b20e25afbc23d
Ryan Kelley <kelleyr@duvalschools.org>
2020-01-16 15:21:40 -0500

    Update kelley_ryan_space_travel_IDLE.py

--- kelley_ryan_space_travel_IDLE.py
+++
@@ -1,4 +0,0 @@
-# Space Travel Calculator, Ryan Kelley, 01/16/20 2:51PM, Version 0.1
-print("This is the Space Travel Calculator. I help determine if you can travel to a certain location in outerspace safely.")  
-name = input("uhhh, what's your name?  ")
-print(name)
