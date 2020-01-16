e0bb058beacc8455770a2729bf82996a49533806
Ryan Kelley <kelleyr@duvalschools.org>
2020-01-16 15:59:34 -0500

    Version 0.1
    Added print() with instructions.

--- kelley_ryan_space_travel_07_02.py
+++ kelley_ryan_space_travel_07_02.py
@@ -1,1 +1,2 @@
-# Space Travel Calculator, Ryan Kelley, 01/16/20 3:41PM, Version 0.0 
+# Space Travel Calculator, Ryan Kelley, 01/16/20 3:41PM, Version 0.1 
+print("Greetings human! I am SpaceTravel Bot-9000.  I will tell you if you will die in outerspace or not.")
--- kelley_ryan_space_travel_IDLE.py
+++ kelley_ryan_space_travel_IDLE.py
@@ -1,1 +1,2 @@
-wertetewtew
+# Space Travel Calculator, Ryan Kelley, 01/16/20 3:41PM, Version 0.0 
+print(" iojdfgoi[adgh4uy8908uy4t9p834yt5934y89fg8oahf78opry    ")
