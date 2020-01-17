917335713b8c613c7546ca8af66137d659c8e1ad
Ryan Kelley <kelleyr@duvalschools.org>
2020-01-17 10:13:30 -0500

    Version 0.1
    Added print() function for instructions.

--- kelley_ryan_space_travel_IDLE.py
+++ kelley_ryan_space_travel_IDLE.py
@@ -1,4 +1,2 @@
-# Space Travel Calculator, Ryan Kelley, 01/16/20 3:41PM, Version 0.1 
-print("Greetings human! I am SpaceTravel Bot-9000.  I will tell you if you will die in outerspace or not.")
-save_name = input("Uhh, what's your name? [Type your first name and press ENTER.]")
-print(save_name)
+# Space Travel Calculator, 01/17/20 9:46AM, Version 0.0
+print("Hello, I am the Space Travel Calculator.  I will determine how long a trip in outerspace will take.")
