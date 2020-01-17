360d7347086c91d7db3ea337b4a3be0c8a192411
Ryan Kelley <kelleyr@duvalschools.org>
2020-01-17 16:06:11 -0500

    Version 0.1
    Added instructions with print() function.

--- kelley_ryan_space_travel_IDLE.py
+++ kelley_ryan_space_travel_IDLE.py
@@ -1,2 +1,2 @@
-# Space Travel Calculator, Ryan Kelley, 01/17/20 2:53PM, Version 0.0 
-print("  ")
+# Space Travel Calculator, Ryan Kelley, 01/17/20 3:42PM, Version 0.0 
+print"kugiugif7i789ty789t87tuig ") 
