bdf905f044614e83e663c2f4eb8bafd1d431401e
Ryan Kelley <kelleyr@duvalschools.org>
2020-01-17 12:17:37 -0500

    Version 0.1
    Used print() to add instructions for the user.

--- kelley_ryan_space_travel.py
+++ kelley_ryan_space_travel.py
@@ -1,3 +1,3 @@
-# Space Travel Calculator, Ryan Kelley, 01/13/20, Version 0.0 
+# Space Travel Calculator, Ryan Kelley, 01/17/20 12:13PM, Version 0.1 
 print("Greetings, I am the NASA Space Travel Calculator.")
 print("I will calculate whether a mission to outerspace will take more or less than three years.")
