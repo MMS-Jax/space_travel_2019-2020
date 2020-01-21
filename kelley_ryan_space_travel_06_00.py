d680cf1e867a66d73309df5093d91f2cdf6b6226
Ryan Kelley <kelleyr@duvalschools.org>
2020-01-21 15:09:21 -0500

    Version 0.2 [Updated]
    Use print() to greet the user with their name.

--- kelley_ryan_space_travel_06_00.py
+++ kelley_ryan_space_travel_06_00.py
@@ -1,4 +1,5 @@
-# Space Travel Calculator, Ryan Kelley, 01/14/20 2:51PM, Version 0.1 
+# Space Travel Calculator, Ryan Kelley, 01/21/20 3:05PM, Version 0.2 
 print("Hello, my name is Bob. I am the NASA Space Travel robot. I will help you calculate space travel times.   ")
-your_name = input("Hello, by what name do you want to be known?")  
-print(your_name)
+your_name = input("Hello, by what name do you want to be known? [Type your name and press ENTER.]")  
+# print(your_name)
+print("Hello!  It is nice to meet you,", your_name,".") 
