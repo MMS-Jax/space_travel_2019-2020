c2f04944724d6a1a4d28215671a1d84ec6e6d11f
Ryan Kelley <kelleyr@duvalschools.org>
2020-01-21 16:10:57 -0500

    Version 0.4
    Added distances inside the solar System.

--- kelley_ryan_space_travel_07_00.py
+++ kelley_ryan_space_travel_07_00.py
@@ -1,4 +1,4 @@
-# Space Travel Calculator, Ryan Kelley, Code Created 01/21/20 3:48PM, Version 0.3
+# Space Travel Calculator, Ryan Kelley, Code Created 01/21/20 4:09PM, Version 0.4
 print("Hello, I am the NASA Space Travel Calculator. I will help you calculate travel times to outer space.")
 user_name = input("Hello human, by what name are you known?")
 print("Hello, it is nice to meet you",user_name,"!")
@@ -7,3 +7,11 @@ print("Hello, it is nice to meet you",user_name,"!")
 million = 1000000
 billion = 1000000000
 trillion = 1000000000000 
+
+# Average Distances Inside Solar System  [Uses Kilometers (Km)] 
+dist_mars = 225 * million 
+print("Mars is only",dist_mars,"kilometers away from the Earth.") 
+dist_venus = 149.5 * million 
+print("Venus is only",dist_venus,"kilometers away from the Earth.") 
+dist_kuiper_belt = 5.95 * billion 
+print("The Kuiper Belt is only",dist_kuiper_belt,"kilometers away from the Earth.") 
--- kelley_ryan_space_travel_IDLE.py
+++ kelley_ryan_space_travel_IDLE.py
@@ -1,4 +1,4 @@
-# Space Travel Calculator, Ryan Kelley, Code Created 01/21/20 3:33PM, Version 0.3
+# Space Travel Calculator, Ryan Kelley, Code Created 01/21/20 3:48PM, Version 0.3
 print("Hello, I am the NASA Space Travel Calculator. I will help you calculate travel times to outer space.")
 user_name = input("Hello human, by what name are you known?")
 print("Hello, it is nice to meet you",user_name,"!")
@@ -7,6 +7,11 @@ print("Hello, it is nice to meet you",user_name,"!")
 million = 1000000
 billion = 1000000000
 trillion = 1000000000000 
-print(534 * million) 
-print(999 / billion)
-print(1745285918 - trillion)
+
+# Average Distances Inside Solar System  [Uses Kilometers (Km)] 
+dist_mars = 225 * million 
+print("Mars is only",dist_mars,"kilometers away from the Earth.") 
+dist_venus = 149.5 * million 
+print("Venus is only",dist_mars,"kilometers away from the Earth.") 
+dist_kuiper_belt = 5.95 * billion 
+print("The Kuiper Belt is only",dist_mars,"kilometers away from the Earth.") 
