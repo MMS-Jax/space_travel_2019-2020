b1e0e44e5f5c30fe52ca06348cb174e35bb463c7
Ryan Kelley <kelleyr@duvalschools.org>
2020-01-21 15:36:00 -0500

    Version 0.3
    Fixed username print() statement.

--- kelley_ryan_space_travel_07_00.py
+++ kelley_ryan_space_travel_07_00.py
@@ -1,4 +1,4 @@
-# Space Travel Calculator, Ryan Kelley, Code Created 01/13/20, Version 0.2
+# Space Travel Calculator, Ryan Kelley, Code Created 01/21/20 3:33PM, Version 0.3
 print("Hello, I am the NASA Space Travel Calculator. I will help you calculate travel times to outer space.")
 user_name = input("Hello human, by what name are you known?")
-print("Hello," + user_name)
+print("Hello, it is nice to meet you",user_name,"!")
--- kelley_ryan_space_travel_IDLE.py
+++ kelley_ryan_space_travel_IDLE.py
@@ -1,14 +1,4 @@
-# Space Travel Calculator, Ryan Kelley, 01/21/20 3:05PM, Version 0.2 
-print("Hello, my name is Bob. I am the NASA Space Travel robot. I will help you calculate space travel times.   ")
-your_name = input("Hello, by what name do you want to be known? [Type your name and press ENTER.]")  
-# print(your_name)
-print("Hello!  It is nice to meet you,", your_name,".") 
-
-# Units of Measure 
-million = 1000000
-billion = 1000000000
-trillion = 1000000000000
-
-print(55 * million)
-print(76 / billion)
-print(23 - trillion) 
+# Space Travel Calculator, Ryan Kelley, Code Created 01/13/20, Version 0.2
+print("Hello, I am the NASA Space Travel Calculator. I will help you calculate travel times to outer space.")
+user_name = input("Hello human, by what name are you known?")
+print("Hello, it is nice to meet you",user_name,"!")
