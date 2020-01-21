20e8ca65485c388f6649a0719eea139c8e4fe804
Ryan Kelley <kelleyr@duvalschools.org>
2020-01-21 10:04:35 -0500

    Version 0.25
    Added variable for user_name, used input() to record data.  print() on line 5 greets them by name.

--- kelley_ryan_space_travel_IDLE.py
+++ kelley_ryan_space_travel_IDLE.py
@@ -1,2 +1,5 @@
-# Space Travel Calculator, Ryan Kelley, 01/17/20 3:42PM, Version 0.0 
-print("kugiugif7i789ty789t87tuig ") 
+# Space Travel Calculator, Ryan Kelley, Code Written 01/14/20 9:53am, Version 0.0 
+print("Hello! I am the Space Travel Calculator.  I will help determine the length of space missions.")
+user_name = input("What is your name? [Type your name and press ENTER.] ") 
+print(user_name) 
+print("Hello, it is good to meet you", user_name , "!  I hope you enjoy using this program.")
