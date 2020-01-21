4c42bb3f5daeb758db7fb67fdb562217c4ee97d0
Ryan Kelley <kelleyr@duvalschools.org>
2020-01-21 12:30:17 -0500

    Version 0.3
    Added units for million, billion, and trillion.

--- kelley_ryan_space_travel_03_01.py
+++ kelley_ryan_space_travel_03_01.py
@@ -1,5 +1,10 @@
-# Space Travel Calculator, Ryan Kelley, Code Written 01/21/20 12:12PM, Version 0.2
+# Space Travel Calculator, Ryan Kelley, Code Written 01/21/20 12:29PM, Version 0.3
 print("Hello, I am the Space-Travel Calcubot.  I will help you determine if a mission will take more or less than three years.")  
 name = input("By what name do you want to be called? [Type your name and press ENTER.]")
 # print(name)
 print("Hello!  It is nice to meet you,",name,".")
+
+# Units of Measurement 
+million = 1000000 # Six Zero's 
+billion = 1000000000 # Nine Zero's 
+trillion = 1000000000000 # Twelve Zero's 
--- kelley_ryan_space_travel_IDLE.py
+++ kelley_ryan_space_travel_IDLE.py
@@ -1,5 +1,13 @@
-# Space Travel Calculator, Ryan Kelley, Code Written 01/14/20 12:16PM, Version 0.1
+# Space Travel Calculator, Ryan Kelley, Code Written 01/21/20 12:12PM, Version 0.2
 print("Hello, I am the Space-Travel Calcubot.  I will help you determine if a mission will take more or less than three years.")  
 name = input("By what name do you want to be called? [Type your name and press ENTER.]")
-print(name)
+# print(name)
 print("Hello!  It is nice to meet you,",name,".")
+
+# Units of Measurement 
+million = 1000000 # Six Zero's 
+billion = 1000000000 # Nine Zero's 
+trillion = 1000000000000 # Twelve Zero's 
+print(542 * billion)
+print(123 / trillion)
+print(999 - million)
