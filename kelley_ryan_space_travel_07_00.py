550e0a6a4d53c658a6c8eca7746580b5c7d3ac5d
Ryan Kelley <kelleyr@duvalschools.org>
2020-01-21 15:50:17 -0500

    Version 0.3
    Added units of measurement.

--- kelley_ryan_space_travel_07_00.py
+++ kelley_ryan_space_travel_07_00.py
@@ -1,4 +1,9 @@
-# Space Travel Calculator, Ryan Kelley, Code Created 01/21/20 3:33PM, Version 0.3
+# Space Travel Calculator, Ryan Kelley, Code Created 01/21/20 3:48PM, Version 0.3
 print("Hello, I am the NASA Space Travel Calculator. I will help you calculate travel times to outer space.")
 user_name = input("Hello human, by what name are you known?")
 print("Hello, it is nice to meet you",user_name,"!")
+
+# Units of Measurement 
+million = 1000000
+billion = 1000000000
+trillion = 1000000000000 
--- kelley_ryan_space_travel_IDLE.py
+++ kelley_ryan_space_travel_IDLE.py
@@ -1,4 +1,12 @@
-# Space Travel Calculator, Ryan Kelley, Code Created 01/13/20, Version 0.2
+# Space Travel Calculator, Ryan Kelley, Code Created 01/21/20 3:33PM, Version 0.3
 print("Hello, I am the NASA Space Travel Calculator. I will help you calculate travel times to outer space.")
 user_name = input("Hello human, by what name are you known?")
 print("Hello, it is nice to meet you",user_name,"!")
+
+# Units of Measurement 
+million = 1000000
+billion = 1000000000
+trillion = 1000000000000 
+print(534 * million) 
+print(999 / billion)
+print(1745285918 - trillion)
