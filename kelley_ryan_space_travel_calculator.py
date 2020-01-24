8850e478c06e09a8a9d8a6c56e759b6c71af3594
Kelley <kelleyr@duvalschools.org>
2020-01-24 11:41:26 -0500

    Version 0.2
    Added print() with instructions, and created variable for user_name with input()

--- kelley_ryan_space_travel_calculator.py
+++ kelley_ryan_space_travel_calculator.py
@@ -1,4 +1,4 @@
-# Space Travel Calculator, Ryan Kelley, 01/24/20 11:18AM, Version 0.1
+# Space Travel Calculator, Ryan Kelley, 01/24/20 11:40AM, Version 0.2
 print("""
 .▄▄ ·  ▄▄▄· ▄▄▄·  ▄▄· ▄▄▄ .                              
 ▐█ ▀. ▐█ ▄█▐█ ▀█ ▐█ ▌▪▀▄.▀·                              
@@ -18,5 +18,8 @@ print("""
                 by
             Ryan Kelley
             January 24, 2020
-            Version 0.1
+            Version 0.2
 """)
+print("This Space Travel Calculator determines if an outerspace mission will take more or less than three years.")
+user_name = input("What is your name? [Please type your name and press ENTER.]  ")
+print("Hello, how are you?  It is nice to meet you", user_name,"!")
