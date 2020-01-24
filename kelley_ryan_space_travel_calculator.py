3b7f241808d786d5b50eae43ef71ed4b90041408
Kelley <kelleyr@duvalschools.org>
2020-01-24 14:21:56 -0500

    Version 0.4a
    Finished distances and added print() functions to display the distances.

--- kelley_ryan_space_travel_calculator.py
+++ kelley_ryan_space_travel_calculator.py
@@ -1,4 +1,4 @@
-# Space Travel Calculator, Ryan Kelley, 01/24/20 2:12PM, Version 0.4
+# Space Travel Calculator, Ryan Kelley, 01/24/20 2:21PM, Version 0.4a
 print("""
 .▄▄ ·  ▄▄▄· ▄▄▄·  ▄▄· ▄▄▄ .                              
 ▐█ ▀. ▐█ ▄█▐█ ▀█ ▐█ ▌▪▀▄.▀·                              
@@ -18,7 +18,7 @@ print("""
                 by
             Ryan Kelley
             January 24, 2020
-            Version 0.4
+            Version 0.4a
 """)
 print("This Space Travel Calculator determines if an outerspace mission will take more or less than three years.")
 user_name = input("What is your name? [Please type your name and press ENTER.]  ")
@@ -30,8 +30,11 @@ billion = 1000000000
 trillion = 1000000000000
 
 # Distances in Solar System [Measured in kilometers.]
+dist_sun = 150 * million
 dist_mars = 225 * million
-dist_??
-dist_?? 
+dist_pluto = 5.89 * billion 
 
+print("The distance to the Sun is",dist_sun," kilometers.")
+print("The distance to Mars is",dist_mars," kilometers.")
+print("The distance to Pluto is",dist_pluto," kilometers.")
 
