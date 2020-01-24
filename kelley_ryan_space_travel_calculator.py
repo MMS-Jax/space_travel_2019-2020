423686b1136bc7f32c495e97df23e2cfd87f707b
Kelley <kelleyr@duvalschools.org>
2020-01-24 14:13:11 -0500

    Version 0.4
    Added distances for objects in our Solar System.

--- kelley_ryan_space_travel_calculator.py
+++ kelley_ryan_space_travel_calculator.py
@@ -1,4 +1,4 @@
-# Space Travel Calculator, Ryan Kelley, 01/24/20 11:45AM, Version 0.3
+# Space Travel Calculator, Ryan Kelley, 01/24/20 2:12PM, Version 0.4
 print("""
 .▄▄ ·  ▄▄▄· ▄▄▄·  ▄▄· ▄▄▄ .                              
 ▐█ ▀. ▐█ ▄█▐█ ▀█ ▐█ ▌▪▀▄.▀·                              
@@ -18,7 +18,7 @@ print("""
                 by
             Ryan Kelley
             January 24, 2020
-            Version 0.3
+            Version 0.4
 """)
 print("This Space Travel Calculator determines if an outerspace mission will take more or less than three years.")
 user_name = input("What is your name? [Please type your name and press ENTER.]  ")
@@ -28,5 +28,10 @@ print("Hello, how are you?  It is nice to meet you", user_name,"!")
 million = 1000000
 billion = 1000000000
 trillion = 1000000000000
+
+# Distances in Solar System [Measured in kilometers.]
+dist_mars = 225 * million
+dist_??
+dist_?? 
 
 
