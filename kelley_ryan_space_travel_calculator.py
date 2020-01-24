ba6575ce6c537294da7360f87f24aed1e9091e7d
Kelley <kelleyr@duvalschools.org>
2020-01-24 11:46:15 -0500

    Version 0.3
    Added units of measurement for million, billion, and trillion.

--- kelley_ryan_space_travel_calculator.py
+++ kelley_ryan_space_travel_calculator.py
@@ -1,4 +1,4 @@
-# Space Travel Calculator, Ryan Kelley, 01/24/20 11:40AM, Version 0.2
+# Space Travel Calculator, Ryan Kelley, 01/24/20 11:45AM, Version 0.3
 print("""
 .▄▄ ·  ▄▄▄· ▄▄▄·  ▄▄· ▄▄▄ .                              
 ▐█ ▀. ▐█ ▄█▐█ ▀█ ▐█ ▌▪▀▄.▀·                              
@@ -18,8 +18,15 @@ print("""
                 by
             Ryan Kelley
             January 24, 2020
-            Version 0.2
+            Version 0.3
 """)
 print("This Space Travel Calculator determines if an outerspace mission will take more or less than three years.")
 user_name = input("What is your name? [Please type your name and press ENTER.]  ")
 print("Hello, how are you?  It is nice to meet you", user_name,"!")
+
+# Units of Measurement
+million = 1000000
+billion = 1000000000
+trillion = 1000000000000
+
+
