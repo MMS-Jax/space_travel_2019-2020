dfe56b0c8645170e024788f846a2d51af5617903
Kelley <kelleyr@duvalschools.org>
2020-01-27 09:12:30 -0500

    Version 0.5
    Added menu for selecting the object to travel to in the software.

--- kelley_ryan_space_travel_calculator.py
+++ kelley_ryan_space_travel_calculator.py
@@ -1,4 +1,4 @@
-# Space Travel Calculator, Ryan Kelley, 01/24/20 2:26PM, Version 0.45
+# Space Travel Calculator, Ryan Kelley, 01/27/20 9:10AM, Version 0.5
 print("""
 .▄▄ ·  ▄▄▄· ▄▄▄·  ▄▄· ▄▄▄ .                              
 ▐█ ▀. ▐█ ▄█▐█ ▀█ ▐█ ▌▪▀▄.▀·                              
@@ -17,8 +17,8 @@ print("""
 ·▀▀▀  ▀  ▀ .▀▀▀ ·▀▀▀  ▀▀▀ .▀▀▀  ▀  ▀  ▀▀▀  ▀█▄▀▪.▀  ▀    
                 by
             Ryan Kelley
-            January 24, 2020
-            Version 0.45
+            January 27, 2020
+            Version 0.5
 """)
 print("This Space Travel Calculator determines if an outerspace mission will take more or less than three years.")
 user_name = input("What is your name? [Please type your name and press ENTER.]  ")
@@ -32,11 +32,7 @@ trillion = 1000000000000
 # Distances in Solar System [Measured in kilometers.]
 dist_sun = 150 * million
 dist_mars = 225 * million
-dist_pluto = 5.89 * billion 
-
-print("The distance to the Sun is",dist_sun," kilometers.")
-print("The distance to Mars is",dist_mars," kilometers.")
-print("The distance to Pluto is",dist_pluto," kilometers.")
+dist_pluto = 5.89 * billion
 
 # Distances Outside of Solar System [Measured in kilometers.]
 light_year = 9460730000000 # Kilometers in a light year.  
@@ -45,6 +41,31 @@ dist_alpha_centauri = 4.3 * light_year
 dist_eps_eridani_b = 10.44 * light_year
 dist_v616_mon = 2800 * light_year
 
-print("The distance to Alpha Centauri is",dist_alpha_centauri," kilometers.")
-print("The distance to the exoplanet Eps Eridani B is",dist_eps_eridani_b," kilometers.")
-print("The distance to the nearest blackholeis",dist_v616_mon," kilometers.")
+print("#------------------------------------------------------------------------#")
+print("# Please select from the following objects:                              #")
+print("#                                                                        #")
+print("# [0] The Sun                                                            #")
+print("# [1] Mars                                                               #")
+print("# [2] Pluto                                                              #")
+print("# [3] Alpha Centauri                                                     #")
+print("# [4] Eps Eridani B                                                      #")
+print("# [5] V616 Mon                                                           #")
+print("#                                                                        #")
+print("# Once you have made your selection this program will determine the      #")
+print("# distance between the Earth and your chosen object.                     #")
+print("#                                                                        #")
+print("# To make your selection, type in the number from the list above, then   #")
+print("# press the ENTER key.                                                   #")
+print("#                                                                        #")
+print("#------------------------------------------------------------------------#")
+
+
+# print("The distance to the Sun is",dist_sun," kilometers.")
+# print("The distance to Mars is",dist_mars," kilometers.")
+# print("The distance to Pluto is",dist_pluto," kilometers.")
+
+
+
+# print("The distance to Alpha Centauri is",dist_alpha_centauri," kilometers.")
+# print("The distance to the exoplanet Eps Eridani B is",dist_eps_eridani_b," kilometers.")
+# print("The distance to the nearest blackhole is",dist_v616_mon," kilometers.")
