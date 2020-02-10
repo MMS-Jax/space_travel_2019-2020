548a44e809552e9db68e3ab40edb7dfd2262e027
Ryan Kelley <kelleyr@duvalschools.org>
2020-02-10 15:19:50 -0500

    Version 0.6
    Added if / else statement for menu choice.

---
+++ group_examples/graco_francis_chen_krawchison_hunt_space_travel_calculator.py
@@ -0,0 +1,90 @@
+# Space Travel Calculator, Ryan Kelley, 02/10/20 3:15PM, Version 0.6
+import time 
+print("""
+.▄▄ ·  ▄▄▄· ▄▄▄·  ▄▄· ▄▄▄ .                              
+▐█ ▀. ▐█ ▄█▐█ ▀█ ▐█ ▌▪▀▄.▀·                              
+▄▀▀▀█▄ ██▀·▄█▀▀█ ██ ▄▄▐▀▀▪▄                              
+▐█▄▪▐█▐█▪·•▐█ ▪▐▌▐███▌▐█▄▄▌                              
+ ▀▀▀▀ .▀    ▀  ▀ ·▀▀▀  ▀▀▀                               
+▄▄▄▄▄▄▄▄   ▄▄▄·  ▌ ▐·▄▄▄ .▄▄▌                            
+•██  ▀▄ █·▐█ ▀█ ▪█·█▌▀▄.▀·██•                            
+ ▐█.▪▐▀▀▄ ▄█▀▀█ ▐█▐█•▐▀▀▪▄██▪                            
+ ▐█▌·▐█•█▌▐█ ▪▐▌ ███ ▐█▄▄▌▐█▌▐▌                          
+ ▀▀▀ .▀  ▀ ▀  ▀ . ▀   ▀▀▀ .▀▀▀                           
+ ▄▄·  ▄▄▄· ▄▄▌   ▄▄· ▄• ▄▌▄▄▌   ▄▄▄· ▄▄▄▄▄      ▄▄▄      
+▐█ ▌▪▐█ ▀█ ██•  ▐█ ▌▪█▪██▌██•  ▐█ ▀█ •██  ▪     ▀▄ █·    
+██ ▄▄▄█▀▀█ ██▪  ██ ▄▄█▌▐█▌██▪  ▄█▀▀█  ▐█.▪ ▄█▀▄ ▐▀▀▄     
+▐███▌▐█ ▪▐▌▐█▌▐▌▐███▌▐█▄█▌▐█▌▐▌▐█ ▪▐▌ ▐█▌·▐█▌.▐▌▐█•█▌    
+·▀▀▀  ▀  ▀ .▀▀▀ ·▀▀▀  ▀▀▀ .▀▀▀  ▀  ▀  ▀▀▀  ▀█▄▀▪.▀  ▀    
+                by
+            Ryan Kelley
+            February 10, 2020
+            Version 0.6
+""")
+print("This Space Travel Calculator determines if an outerspace mission will take more or less than three years.")
+user_name = input("What is your name? [Please type your name and press ENTER.]  ")
+print("Hello, how are you?  It is nice to meet you", user_name,"!")
+#time.sleep(3)
+
+# Units of Measurement
+million = 1000000
+billion = 1000000000
+trillion = 1000000000000
+
+# Distances in Solar System [Measured in kilometers.]
+dist_sun = 150 * million
+dist_mars = 225 * million
+dist_pluto = 5.89 * billion
+
+# Distances Outside of Solar System [Measured in kilometers.]
+light_year = 9460730000000 # Kilometers in a light year.  
+
+dist_alpha_centauri = 4.3 * light_year
+dist_eps_eridani_b = 10.44 * light_year
+dist_v616_mon = 2800 * light_year
+
+print("#------------------------------------------------------------------------#")
+print("# Please select from the following objects:                              #")
+print("#                                                                        #")
+print("# [0] The Sun                                                            #")
+print("# [1] Mars                                                               #")
+print("# [2] Pluto                                                              #")
+print("# [3] Alpha Centauri                                                     #")
+print("# [4] Eps Eridani B                                                      #")
+print("# [5] V616 Mon                                                           #")
+print("#                                                                        #")
+print("# Once you have made your selection this program will determine the      #")
+print("# distance between the Earth and your chosen object.                     #")
+print("#                                                                        #")
+print("# To make your selection, type in the number from the list above, then   #")
+print("# press the ENTER key.                                                   #")
+print("#------------------------------------------------------------------------#")
+menu_choice = input("Please make a selection.")
+menu_choice = int(menu_choice)
+time.sleep(3)
+
+distance = 0 
+if menu_choice == 0: # Is the menu_choice variable equal to 0? If so, do the following:
+    distance = dist_sun
+    print("Earth is currently",distance,"kilometers from the Sun.")
+elif menu_choice == 1:
+    distance = dist_mars
+    print("Earth is currently",distance,"kilometers from Mars.")
+elif menu_choice == 2:
+    distance = dist_pluto
+    print("Earth is currently",distance,"kilometers from Pluto.")
+elif menu_choice == 3:
+    distance = dist_alpha_centauri
+    print("Earth is currently",distance,"kilometers from Alpha Centauri.")
+elif menu_choice == 4:
+    distance = dist_eps_eridani_b
+    print("Earth is currently",distance,"kilometers from the exoplanet Eps Eridani B.")
+elif menu_choice == 5:
+    distance = dist_v616_mon
+    print("Earth is currently",distance,"kilometers from the blackhole V616 Mon.")
+else:
+    print("You did not choose a number from the menu!  Please restart the program.")
+    exit()
+
+
+
